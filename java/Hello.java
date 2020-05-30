46db89b61ccb59c7647939261e42b39c877aa5e1
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-05-30 05:30:43 +0530

    Add files via upload

---
+++ java/Hello.java
@@ -0,0 +1,9 @@
+class Hello extends hyllo
+{
+    public static void main(String args[])
+    {
+        System.out.println("rahul");
+        System.out.print("tem");
+        System.out.println("hello");
+    }
+}
