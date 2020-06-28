7800b82e59bc5244b9c46a4f3e0151f4c6f6a9f2
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-06-28 17:37:48 +0530

    Adder

---
+++ java/Hackerrank (Basic) Certification/Adder.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+abstract class Calculator {
+    abstract int add(int a, int b);
+}
+
+class Adder extends Calculator{
+
+    int a,b;
+
+    Adder(){
+        super();
+    }
+
+    Adder(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    int add(int a, int b){
+        return a+b;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        int a, b;
+        try (Scanner scan = new Scanner(System.in)) {
+            a = scan.nextInt();
+            b = scan.nextInt();
+        }
+
+        Calculator adderObject = new Adder();
+        System.out.println("The sum is: " + adderObject.add(a, b));
+    }
+}
