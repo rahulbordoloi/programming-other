f11b8e5a0510b49902fe40df12b02ec259d1bca1
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-06-28 07:26:10 +0530

    temp.js

---
+++ js/temp.js
@@ -0,0 +1,16 @@
+// var myName = prompt("What's your Name?");
+
+// //console.log(myName)
+
+// alert("My Name is " + myName + "!!!");
+
+//alert(prompt("Enter your Tweet: ").slice(0,140));
+
+// var x = prompt("Enter your Name");
+// x = x.slice(0,1).toUpperCase() + x.slice(1, x.length);
+// alert("Your Name is " + x + " !");
+
+var x = prompt("Enter your Weight")
+var y = prompt("Enter your Height")
+bmi = x/(y*y)
+alert("Your BMI is " + bmi)
