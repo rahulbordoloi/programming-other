58b06c4b3db5a4f19472b0fb6aee9acdef8ce363
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-07-07 08:26:07 +0530

    Looping and Skipping

---
+++ Bash/Looping and Skipping.sh
@@ -0,0 +1,46 @@
+<<COMMENT 
+Your task is to use for loops to display only odd natural numbers from  to .
+
+Input Format
+
+There is no input.
+
+Constraints
+
+-
+
+Output Format
+
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+Sample Input
+
+-
+
+Sample Output
+
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+Explanation
+
+-
+COMMENT
+
+for (( i = 1; i < 100; i=i+2 )) 
+    do 
+        echo $i  
+    done
