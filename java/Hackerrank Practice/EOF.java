43df20e54c28bcda87cceb7e2121a1202f1f5aaa
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-09-09 08:05:25 +0530

    EOF

---
+++ java/Hackerrank Practice/EOF.java
@@ -0,0 +1,16 @@
+// https://www.hackerrank.com/challenges/java-end-of-file/problem?isFullScreen=true
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int line_num = 0;
+        while(sc.hasNext())
+            System.out.println(++line_num + " " + sc.nextLine());
+        sc.close();
+    }
+}
