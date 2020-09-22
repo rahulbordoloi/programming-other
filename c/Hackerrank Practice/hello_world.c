39f78c9a18f18a486a5e9835e10a19b5ef2d0e72
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-09-22 23:29:57 +0530

    Hello World Problem

---
+++ c/Hackerrank Practice/hello_world.c
@@ -0,0 +1,18 @@
+// Link - https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    printf("%s\n", "Hello, World!");
+    printf("%s", s); 
+    return 0;
+}
