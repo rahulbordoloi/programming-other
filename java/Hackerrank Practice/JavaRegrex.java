1bb97ae63b3860faef907e4c4206c864c67b8596
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-10-18 17:14:08 +0530

    Java Regrex

---
+++ java/Hackerrank Practice/JavaRegrex.java
@@ -0,0 +1,23 @@
+package com.hackerrank;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class JavaRegrex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+
+    String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+}
