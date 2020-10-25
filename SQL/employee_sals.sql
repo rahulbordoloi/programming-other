6b2d677f5e788a38dd72f131e8101ca3fbb696ae
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-10-25 15:37:37 +0530

    Employee Salaries Problem

---
+++ SQL/employee_sals.sql
@@ -0,0 +1,3 @@
+# Link - https://www.hackerrank.com/challenges/salary-of-employees/problem?isFullScreen=true
+
+SELECT name FROM Employee WHERE salary > 2000 AND months < 10 ORDER BY employee_id ASC;
