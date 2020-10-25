ed7d32001ffd33f900e8733876a66356111570d9
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-10-25 15:34:29 +0530

    Weather Observation Station 6 Problem

---
+++ SQL/weather_obs6.sql
@@ -0,0 +1,3 @@
+# Link - https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+SELECT DISTINCT(CITY) FROM STATION WHERE CITY REGEXP '^[aeiou]';
