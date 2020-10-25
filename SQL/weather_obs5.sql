35a5f55f38df8c2700de84d03febc29119c6b99e
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-10-25 13:55:28 +0530

    Weather Observation Station 5 Problem

---
+++ SQL/weather_obs5.sql
@@ -0,0 +1,4 @@
+# Link - https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=true
+
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) ASC, CITY LIMIT 1;
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY LIMIT 1; 
